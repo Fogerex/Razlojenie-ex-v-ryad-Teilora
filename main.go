02ad3776c26518b4150f7ac290da99bd04416256
Fogerex <110209480+Fogerex@users.noreply.github.com>
2022-09-07 00:44:05 +0300

    Update main.go

--- main.go
+++ main.go
@@ -7,28 +7,28 @@ import (
 
 func main() {
 	var (
-		e  float64 = 1 //Степень
-		t  float64 = 5 //Точность
+		e  float64     //= 1 //Степень
+		t  float64     //= 5 //Точность
 		ta float64 = 1 //ta верхняя часть уравнения для а
 		la float64 = 1 //la нижняя часть уравнения для а
-		a  float64 = 1 //Необходимое зло
+		a  float64 = 1 //Отношение ta / la
+		n  int     = 1 //Факториал
 	)
 	fmt.Println("Введи Степень:")
 	fmt.Scan(&e)
 	fmt.Println("Введи Точность:")
 	fmt.Scan(&t)
 	t = math.Pow(0.1, t)
-	n := 1.0
 	for a > t {
 		n++
 		ta++
-		la *= n
+		la *= float64(n)
 		a = ta / la
 		e += a
-		fmt.Printf("n: %f; ta: %f; la: %f; a: %f; e: %f\n", n, ta, la, a, e)
+		fmt.Printf("n: %d; ta: %f; la: %f; a: %f; e: %v\n", n, ta, la, a, e)
 
 	}
-	fmt.Printf("e=%f при точности = %f и n=%f", e, t, n)
-	fmt.Printf("\nEND n: %f; ta: %f; la: %f; a: %f; e: %f", n, ta, la, a, e)
+	fmt.Printf("e=%v при точности = %f и n=%d. Проверочная e вычесленная ПК: %f", e, t, n, math.E)
+	//fmt.Printf("\nEND n: %d; ta: %f; la: %f; a: %f; e: %f", n, ta, la, a, e)
 
 }
