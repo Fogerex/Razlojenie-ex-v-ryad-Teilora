1be26078bf378769180caad54849cd9e979f1d40
Fogerex <110209480+Fogerex@users.noreply.github.com>
2022-09-07 00:09:30 +0300

    Update main.go

--- main.go
+++ main.go
@@ -2,43 +2,30 @@ package main
 
 import (
 	"fmt"
-	"math"
+	_ "fmt"
+	_ "math"
 )
 
 func main() {
-
-	// Разложение ex в ряд Тейлора
-	// Задача:
-	// Используя разложение в ряд Тейлора найти значение ex с заданной точностью e.
-	// Описание:
-	// Код:
-
 	var (
-		x float64
-		n float64
+		e  int     = 1
+		t  float64 = 0.001
+		ta float64 = 1
+		la float64 = 1
+		a  float64 = 1
 	)
-
-	fmt.Println("Введи x:")
-	fmt.Scan(&x)
-	fmt.Println("Введи n:")
-	fmt.Scan(&n)
-	stx := x    //{x в степени}
-	fakt := 1.0 //{значение факториала в знаменателе}
-	ex := 1.0   //{первый элемент в разложении}
-	i := 1.0    //{счётчик}
-	//e := math.E //{число e}
-	//fmt.Println("e:", e)
-
-	for (stx / fakt) >= n {
-		ex += stx / fakt
-		i++
-		stx *= x
-		fakt *= i
-		fmt.Println("i:", i)
-
+	n := 1
+	for a > t {
+		n++
+		ta++
+		la *= float64(n)
+		a = ta / la
+		e += int(a)
+		fmt.Printf("n: %d; ta: %f; la: %f; a: %f; e: %d\n", n, ta, la, a, e)
+		// if a > t {
+		// 	continue
+		// }
 	}
-	fmt.Println("Количество элементов в разложении = ", i+1)
-	fmt.Println("Значение e^", x, "= ", ex)
-	fmt.Println("Значение компьютера: ", math.Exp(x))
+	fmt.Printf("\nEND n: %d; ta: %f; la: %f; a: %f; e: %d", n, ta, la, a, e)
 
 }
