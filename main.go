1291e291854d5e08cf392a1c6fb0b42b90190aee
Fogerex <110209480+Fogerex@users.noreply.github.com>
2022-09-06 22:36:20 +0300

    Update main.go

--- main.go
+++ main.go
@@ -1,5 +1,36 @@
 package main
 
+import (
+	"fmt"
+	"math"
+)
+
 func main() {
+	var (
+		x float64
+		n float64
+	)
+
+	fmt.Println("Введи x:")
+	fmt.Scan(&x)
+	fmt.Println("Введи n:")
+	fmt.Scan(&n)
+	stx := x    //{x в степени}
+	fakt := 1.0 //{значение факториала в знаменателе}
+	ex := 1.0   //{первый элемент в разложении}
+	n = 1.0     //{счётчик}
+	e := math.E //{число e}
+	fmt.Println("e:", e)
+
+	for (stx / fakt) >= e {
+		ex += stx / fakt
+		n++
+		stx *= x
+		fakt *= n
+
+	}
+	fmt.Println("Количество элементов в разложении = ", n+1)
+	fmt.Println("Значение e^", x, "= ", ex)
+	fmt.Println("Значение компьютера: ", math.Exp(x))
 
 }
